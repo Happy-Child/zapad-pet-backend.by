1b74eb416ad6c8a49ea066e97ce2c9aa4808cba5
Egor Lazuka <mozerqake1999@gmail.com>
2021-09-05 18:45:14 +0300

    create typeorm connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ExceptionsModule } from '@app/exceptions';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormconfig from '../ormconfig';
 
 @Module({
-  imports: [ExceptionsModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...ormconfig,
+      migrationsRun: true,
+    }),
+    ExceptionsModule.forRoot(),
+  ],
   providers: [
     {
       provide: APP_INTERCEPTOR,
