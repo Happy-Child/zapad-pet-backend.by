57c1f81a8fc90cf1575369110ad28774744321cd
Egor Lazuka <mozerqake1999@gmail.com>
2021-11-27 17:09:56 +0300

    impl part of tests for POST/DELETE routes district module

---
+++ test/districts/districts-update/districts-update.e2e-spec.ts
@@ -0,0 +1,64 @@
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { TEST_TIMEOUT } from '@app/constants/tests.constants';
+import { JwtService } from '@nestjs/jwt';
+import {
+  bootstrapTestApp,
+  getTestAccessTokensByRoles,
+} from '../../test.helpers';
+import { COOKIE } from '../../../src/modules/auth/constants';
+import { USER_ROLES } from '@app/constants';
+
+describe('DistrictsModule (e2e)', () => {
+  let app: INestApplication;
+  let accessTokensByRoles: Record<USER_ROLES, string>;
+
+  beforeAll(async () => {
+    app = await bootstrapTestApp();
+    accessTokensByRoles = getTestAccessTokensByRoles(app.get(JwtService));
+  });
+
+  describe('POST /districts/:id/leader/:userId', () => {
+    const API_URL = '/districts/:id/leader/:userId';
+
+    it(
+      'should be forbidden all roles except master',
+      () => {
+        const server = app.getHttpServer();
+
+        const requests = Object.keys(accessTokensByRoles)
+          .filter((role) => role !== USER_ROLES.MASTER)
+          .map((role) =>
+            request(server)
+              .post(API_URL.replace(':id', '1').replace(':userId', '1'))
+              .set(
+                'Cookie',
+                `${COOKIE.ACCESS_TOKEN}=${
+                  accessTokensByRoles[role as USER_ROLES]
+                };`,
+              )
+              .expect(({ status }) => {
+                expect(status).toBe(HttpStatus.FORBIDDEN);
+              }),
+          );
+
+        return Promise.all(requests);
+      },
+      TEST_TIMEOUT,
+    );
+
+    // TODO impl tests
+    it.skip(
+      'should be forbidden',
+      () => {
+        // const server = app.getHttpServer();
+        //
+      },
+      TEST_TIMEOUT,
+    );
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
